5c9f9d6e548ff91576ee9d96cbc8a3c287182bc6
S-DHARSAN <dharsanpmp@gmail.com>
2024-01-03 12:25:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,55 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'demo';
+
+  array:any =[
+    {
+      id: "101",
+      name:"Mark",
+      designation:"TL",
+      address:"Chennai",
+      salary:"50000"
+    },
+    {
+      id: "102",
+      name:"Antony",
+      designation:"Manager",
+      address:"Chennai",
+      salary:"75000"
+    },
+    {
+      id: "106",
+      name:"Sathi",
+      designation:"SE",
+      address:"Salem",
+      salary:"40000"
+    },
+    {
+      id: "103",
+      name:"Karthi",
+      designation:"SE",
+      address:"Erode",
+      salary:"40000"
+    },
+    {
+      id: "104",
+      name:"Hari",
+      designation:"AM",
+      address:"Coimbatore",
+      salary:"60000"
+    },
+    {
+      id: "105",
+      name:"Guru",
+      designation:"SE",
+      address:"Madurai",
+      salary:"40000"
+    }
+  ];
+
+  showMe:boolean=true
+  toogle()
+  {
+    this.showMe=!this.showMe
+  }
 }
